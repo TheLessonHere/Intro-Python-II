fd22e8973869562874bdfd993e9949a9921627e0
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-12-04 16:05:10 -0700

    Day 2 MVP

--- src/room.py
+++ src/room.py
@@ -1,5 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 
 class Room:
     def __init__(self, name, description, n_to, s_to, e_to, w_to):
@@ -9,6 +10,19 @@ class Room:
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
+        self.items = []
+
+    def get_items (self):
+        return self.items
+
+    def add_item (self, item):
+        return self.items.append(item)
+
+    def take_item (self, item):
+        return self.items.remove(item)
+
+    def __repr__ (self):
+        return (F"Name: {self.name}, Description: {self.description}, Items: {str(self.items)}")
 
     def __str__ (self):
-        return (F"Name: {self.name}, Description: {self.description}")
+        return (F"Name: {self.name}, Description: {self.description}, Items: {str(self.items)}")
